3ce3d81e98820ca5fe25685199c643f159d16838
Mateusz <marchewka.matthew@gmail.com>
2022-03-02 23:32:00 +0100

    add variables

--- src/pages/articlePage/ArticlePage.tsx
+++ src/pages/articlePage/ArticlePage.tsx
@@ -8,7 +8,7 @@ export const ArticlePage: React.FC<ArticlePageProps> = (props) => {
   const { title } = props;
   return (
     <article className="bg-slate-100 dark:bg-slate-700 text-slate-900 dark:text-slate-50">
-      <div className="relative w-full max-h-72">
+      <div className="relative w-full max-h-300">
         <img
           src="https://picsum.photos/id/10/1920/1080"
           alt=""
@@ -16,10 +16,10 @@ export const ArticlePage: React.FC<ArticlePageProps> = (props) => {
         />
         <div className="container w-full aspect-video" />
       </div>
-      <div className="container mx-auto px-8 lg:px-10 py-8 lg:flex lg:flex-wrap lg:gap-x-10 space-y-10">
+      <div className="container mx-auto px-32 lg:px-40 py-32 lg:flex lg:flex-wrap lg:gap-x-40 space-y-40">
         <div>
           <h1 className="text-3xl">{title}</h1>
-          <div className="prose dark:prose-invert mt-8 lg:mt-10">
+          <div className="prose dark:prose-invert mt-32 lg:mt-40">
             <p>
               For years parents have espoused the health benefits of eating
               garlic bread with cheese to their children, with the food earning
@@ -35,7 +35,7 @@ export const ArticlePage: React.FC<ArticlePageProps> = (props) => {
             </p>
           </div>
         </div>
-        <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-8 h-96 lg:flex-1">
+        <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-32 h-300 lg:flex-1">
           <div className="relative rounded-lg shadow-lg overflow-hidden">
             <img
               src="https://picsum.photos/id/10/1920/1080"
@@ -76,7 +76,7 @@ export const ArticlePage: React.FC<ArticlePageProps> = (props) => {
             </div>
           </div>
         </div>
-        <div className="bg-white shadow-xl w-full p-8 rounded-lg font-bold">
+        <div className="bg-white shadow-xl w-full p-32 rounded-lg font-bold">
           Extra content
         </div>
       </div>
