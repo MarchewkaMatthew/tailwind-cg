6068504a0ff4f531849835f98e5e7dbeb05e53d4
Mateusz <marchewka.matthew@gmail.com>
2022-03-03 11:55:13 +0100

    skończony layout

--- src/pages/articlePage/ArticlePage.tsx
+++ src/pages/articlePage/ArticlePage.tsx
@@ -11,16 +11,28 @@ interface ArticlePageProps {
 export const ArticlePage: React.FC<ArticlePageProps> = (props) => {
   const { title } = props;
   return (
-    <article>
+    <article className="bg-slate-100 dark:bg-slate-700 text-slate-900 dark:text-slate-50 min-h-screen">
       <ArticleHero />
-      <h1>{title}</h1>
-      <ArticleRichtext />
-      <GalleryItem />
-      <GalleryItem />
-      <GalleryItem />
-      <GalleryItem />
-      <GalleryItem />
-      <Card>Extra content</Card>
+      <div className="container mx-auto px-32 lg:px-40 py-32 lg:flex lg:flex-wrap lg:gap-x-40 space-y-40">
+        <div>
+          <h1 className="text-3xl">{title}</h1>
+          <div className="prose dark:prose-invert mt-32 lg:mt-40">
+            <ArticleRichtext />
+          </div>
+        </div>
+        <div className="grid grid-flow-col grid-rows-2 grid-cols-3 gap-32 h-300 lg:flex-1">
+          <GalleryItem />
+          <div className="col-start-3">
+            <GalleryItem />
+          </div>
+          <GalleryItem />
+          <GalleryItem />
+          <div className="row-start-1 col-start-2 col-span-2">
+            <GalleryItem />
+          </div>
+        </div>
+        <Card>Extra content</Card>
+      </div>
     </article>
   );
 };
