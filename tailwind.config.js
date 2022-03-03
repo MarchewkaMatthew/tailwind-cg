1d430999c8202adc1984243c10c38d5dee68a81c
Mateusz <marchewka.matthew@gmail.com>
2022-03-03 11:47:27 +0100

    dodanie zmiennych

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    spacing: {
+      0: "0px",
+      32: "32px",
+      40: "40px",
+      300: "300px",
+    },
     extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
